f2aba61df452b70beb41805d45c22433b592409d
AffanDN <affan03nusli@gmail.com>
2024-04-18 11:35:02 +0700

    Initial Commit

---
+++ MontraDevApp/MontraDevAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MontraDevAppApp.swift
+//  MontraDevApp
+//
+//  Created by Macbook Pro on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MontraDevAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
